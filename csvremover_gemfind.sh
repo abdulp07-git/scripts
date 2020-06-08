b2460a4d8fbef368cc9eadb98c4c3c9500651558
abdulp07-git <servers@gemfind.com>
2020-06-08 20:38:18 +0530

    csv removal final script

--- csvremover_gemfind.sh
+++ csvremover_gemfind.sh
@@ -7,23 +7,28 @@ find /home/*/public_html -maxdepth 1 -type d \( -name "gemfind" -o -name "sync_g
 		cd /home/$i/public_html
                 website=`grep  $i$ /etc/trueuserdomains | cut -d : -f1`
 	        	if [ -f "app/etc/local.xml" ]; then
-				echo "$website >>> MAGENTO"
-				echo "Common files:"
-				echo "============="
+				echo "$website >>> MAGENTO" >> /home/CSVREMOVER/report.txt
+				echo "Common files:" >> /home/CSVREMOVER/report.txt
+				echo "=============" >> /home/CSVREMOVER/report.txt
 				cd gemfind
 				ls pending/ | sort > file1
 				ls imported/ | sort > file2
 				comm -12 file1 file2 > common
-			        cat common >> /home/test/csvremoved.$(date +%Y-%m-%d).log	
+			        cat common >> /home/CSVREMOVER/report.txt
+					for j in `cat common`;do rm -rf ./pending/$i ./imported/$i;done;
 			else
-                        	echo "$website >>> WORDPRESS"
-				echo "Common files:"
-                                echo "============="
+                        	echo "$website >>> WORDPRESS" >> /home/CSVREMOVER/report.txt
+				echo "Common files:" >> /home/CSVREMOVER/report.txt
+                                echo "=============" >> /home/CSVREMOVER/report.txt
 				cd sync_gemfind
 				ls pending/ | sort > file1
 				ls completed/ | sort > file2
 				comm -12 file1 file2 > common
-				cat common >> /home/test/csvremoved.$(date +%Y-%m-%d).log
+				cat common >> /home/CSVREMOVER/report.txt
+					for j in `cat common`;do rm -rf ./pending/$i ./completed/$i;done;
 		       
                         fi		
         done
+
+mail -s "CSV files removed DEV Server" tvcfred@gmail.com < /home/CSVREMOVER/report.txt
+rm -f /home/CSVREMOVER/report.txt
